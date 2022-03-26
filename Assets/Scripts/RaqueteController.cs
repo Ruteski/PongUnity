ab0f2596550b8dc4ee17c45f16da10ae60a41fa7
Lincoln Ruteski <lruteski@gmail.com>
2022-03-26 15:13:11 -0300

    impedindo que a raquete saia da tela

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -6,7 +6,8 @@ public class RaqueteController : MonoBehaviour
 {
     private Vector3 minhaPosicao;
     public float meuY;
-    public float velocidade = 5f;
+    public float velocidade = 7f;
+    public float meuLimite = 3.5f;
 
     // Start is called before the first frame update
     void Start() 
@@ -20,12 +21,14 @@ public class RaqueteController : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow)) {
             //meuY += 0.005f;
 
-            //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
-            meuY += velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
-            
+            //verifica o limite da tela
+            if (meuY < meuLimite) {
+                //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
+                meuY += velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+            }
         } 
         
-        if (Input.GetKey(KeyCode.DownArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) && (meuY > -meuLimite)) {
             //meuY -= 0.005f;
 
             //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
