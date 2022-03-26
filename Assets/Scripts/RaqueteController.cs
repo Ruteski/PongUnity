26b5bfa32415239b2fdf9bbf20e38e5fbbbfe2ad
Lincoln Ruteski <lruteski@gmail.com>
2022-03-26 15:04:43 -0300

    movimentacao da raquete cima e baixo

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class RaqueteController : MonoBehaviour
 {
-
-
+    private Vector3 minhaPosicao;
+    public float meuY;
+    public float velocidade = 5f;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        //pegando a posicao inicial da raquete e aplicando a minha posicao
+        minhaPosicao = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            //meuY += 0.005f;
+
+            //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
+            meuY += velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+            
+        } 
         
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            //meuY -= 0.005f;
+
+            //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
+            meuY -= velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+        }
+
+        minhaPosicao.y = meuY;
+        transform.position = minhaPosicao;
     }
+
 }
