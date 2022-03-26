3091e09ce93ce335016faaa145908ec0bcee4474
Lincoln Ruteski <lruteski@gmail.com>
2022-03-26 16:49:24 -0300

    adicionando o player2 e programando ele

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -18,21 +18,34 @@ public class RaqueteController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            //meuY += 0.005f;
 
-            //verifica o limite da tela
-            if (meuY < meuLimite) {
-                //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
-                meuY += velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+        if (gameObject.tag == "Player1") {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                //meuY += 0.005f;
+
+                //verifica o limite da tela
+                if (meuY < meuLimite) {
+                    //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
+                    meuY += velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+                }
             }
-        } 
-        
-        if (Input.GetKey(KeyCode.DownArrow) && (meuY > -meuLimite)) {
-            //meuY -= 0.005f;
 
-            //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
-            meuY -= velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+            if (Input.GetKey(KeyCode.DownArrow) && (meuY > -meuLimite)) {
+                //meuY -= 0.005f;
+
+                //deltaTime retorna o tempo que leva para ir de um frame ate o proximo frame
+                meuY -= velocidade * Time.deltaTime; // usando o deltaTime pra manter a msm velocidade em diferentes FPS
+            }
+        } else {
+            if (Input.GetKey(KeyCode.W)) {
+                if (meuY < meuLimite) {
+                    meuY += velocidade * Time.deltaTime;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.S) && (meuY > -meuLimite)) {
+                meuY -= velocidade * Time.deltaTime;
+            }
         }
 
         minhaPosicao.y = meuY;
