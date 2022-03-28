e11dff99d783c76a004d29af4352a36e3e724911
Lincoln Ruteski <lruteski@gmail.com>
2022-03-28 17:55:06 -0300

    recarregando a cena, quando a bola sair fora do campo

--- Assets/Scripts/BolaController.cs
+++ Assets/Scripts/BolaController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BolaController : MonoBehaviour
 {
     public Rigidbody2D meuRB;
     private Vector2 minhaVelocidade;
     public float velocidade = 5f;
+    public float limiteHorizontal = 12f;
 
     private Vector2 DirecaoInicial(float vel1, float vel2) {
         return new Vector2 (vel1, vel2);
@@ -36,6 +38,10 @@ public class BolaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //checa se a bola saiu da cena
+        if (transform.position.x <= -limiteHorizontal || transform.position.x >= limiteHorizontal) {
+            //recarrega a cena
+            SceneManager.LoadScene("Jogo");
+        }
     }
 }
